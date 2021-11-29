218395ba0e627c12084e38d5551a34ab4a2badc2
ibtsam018 <ibtsamahmad8@gmail.com>
2021-11-30 02:11:33 +0500

    remove pywinsdsdadsdadsdasdsadsadsadfsfdsdsddsadsdsadsadsaadsdasdadsadsadewresd

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-i$x=7lh-#%g@b5v@eyh1z&c%ziml5s708r#8xn+6i)jcvdjju7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
